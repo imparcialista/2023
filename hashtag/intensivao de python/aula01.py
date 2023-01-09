1cd498900417be9f678cdcc4d9780c4dc949ceb4
imparcialista <lucasaraujoz13@outlook.com>
2023-01-09 20:20:11 -0300

    Create aula01.py

---
+++ hashtag/intensivao de python/aula01.py
@@ -0,0 +1,7 @@
+print('Olá mundo')
+
+# passo 1: entrar no sistema da empresa (no link do drive, ou no site do programa)
+# passo 2: navegar até o local do relatório (entrar na pasta do explorer)
+# passo 3: exportar o relatório (fazer download)
+# passo 4: calcular os indicadores (faturamento e quantidade de produtos)
+# passo 5: enviar um e-mail para diretoria
