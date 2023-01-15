85753c4ce71f007a92f6cf31c98e75489717b3cf
imparcialista <lucasaraujoz13@outlook.com>
2023-01-15 13:07:07 -0300

    15012023

--- hashtag/Intensivão de Python/aula02/aula02.py
+++ hashtag/Intensivão de Python/aula02/aula02.py
@@ -6,7 +6,7 @@ tabela = pd.read_csv("telecom_users.csv")
 
 # Passo 2: Visualizar a base de dados
 tabela = tabela.drop("Unnamed: 0", axis=1)
-#display(tabela)
+print(tabela)
 # - Entender quais as informações tão disponíveis
 # - Descobrir as cagadas da base de dados
 
